b55757f2b2b69fd11f80a3a74a8a8b847e9800f8
shobuj <shobujsa93@gmail.com>
2022-06-30 16:00:22 +0600

    category crud completed

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Category;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
+use App\Http\Requests\StoreCategoryRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -24,5 +25,19 @@ class CategoryController extends Controller
 
         $category = Category::create($request->all());
         return new CategoryResource($category);
+    }
+
+    public function update(Category $category, StoreCategoryRequest $request)
+    {
+        $category->update($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }

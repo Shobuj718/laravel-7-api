1e4152383d82648098781f0087f0c5e574a87b92
shobuj <shobujsa93@gmail.com>
2022-06-30 12:51:00 +0600

    added vue components

---
+++ resources/js/components/Front.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container" :class="{'loading': loading}">
+        <div class="row">
+            <div class="col-lg-3">
+                <h1 class="my-4">Shop Catalogs</h1>
+                <div class="list-group">
+                    <a class="list-group-item" v-for="category in categories">
+                        {{ category.name }}
+                    </a>
+                </div>
+
+            </div>
+            <div class="col-lg-9">
+                <div class="row mt-4">
+                    <div class="col-lg-4 col-md-6 mb-4" v-for="product in products.data">
+                        <div class="card h-100">
+                            <a href="#">
+                                <img class="card-img-top" src="http://placehold.it/700x400" alt="">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="#">{{ product.name }}</a>
+                                </h4>
+                                <h5>$ {{ product.price }}</h5>
+                                <p class="card-text">{{ product.description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <pagination :data="products" @pagination-change-page="loadProducts"></pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                categories: [],
+                products: {},
+                loading: true,
+            }
+        },
+
+        created() {
+            this.loadCategories();
+            this.loadProducts();
+        },
+
+        mounted(){
+            cnosole.log('mounted');
+            
+        },
+
+
+        methods: {
+            loadCategories: function () {
+                axios.get('/api/categories')
+                    .then((response) => {
+                        this.categories = response.data.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+
+            loadProducts(page = 1) {
+                axios.get('/api/products?page=' + page)
+                    .then((response) => {
+                        this.products = response.data;
+                        this.loading = false;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::view('/', 'home');
+
+Route::view('categories/create', 'categories.create');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+

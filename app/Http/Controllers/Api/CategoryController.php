c6fa860d579cdedca9a23af9da98b30d3bf992d4
shobuj <shobujsa93@gmail.com>
2022-06-30 11:06:33 +0600

    category resource added

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return Category::all();
+        $categories =  Category::select('id', 'name', 'created_at')->get();
+        return CategoryResource::collection($categories);
+
+    }
+
+    public function show(Category $category){
+        return new CategoryResource($category);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('categories', 'Api\CategoryController@index');
+Route::get('categories/{category}', 'Api\CategoryController@show');
